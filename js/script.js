a02d8f379a822bec014424d78f0db56fb460bc58
Edoardo Di Vitale <edv.lavoro@gmail.com>
2020-05-25 18:12:43 +0200

    Up

--- js/script.js
+++ js/script.js
@@ -1,21 +1,28 @@
 var nomi = ["Luca", "luca", "marco", "Marco", "Paolo", "paolo", "Alessandro", "alessandro", "GIALLLUCA"];
 var nome = prompt('Chi Sei?>_>');
+var presente = false;
 var dadi = document.getElementById('dadi');
+var getOut = document.getElementById('get-out');
 for (i = 0; i < nomi.length; i++) {
   if (nome == nomi[i]) {
-    dadi.style.display = 'block';
+    presente = true;
   }
 };
-
+  if (presente) {
+    dadi.style.display = 'block';
+  }
+  else {
+    getOut.style.display = 'block';
+  }
 var dado = document.getElementById('dado');
-var risultato = document.getElementById('risultato');
-var risultatoBot = document.getElementById('risultatoBot');
-var risltCasuale = Math.floor(Math.random() * 6) + 1;
-var risltCasualeBot = Math.floor(Math.random() * 6) + 1;
-var vincitore = document.getElementById('vincitore');
 
 dado.addEventListener('click',
 function() {
+  var risultato = document.getElementById('risultato');
+  var risultatoBot = document.getElementById('risultatoBot');
+  var risltCasuale = Math.floor(Math.random() * 6) + 1;
+  var risltCasualeBot = Math.floor(Math.random() * 6) + 1;
+  var vincitore = document.getElementById('vincitore');
   risultato.innerHTML = 'Il tuo Risultato: ' + risltCasuale;
   risultatoBot.innerHTML = 'Risultato del tuo avversario: ' + risltCasualeBot;
   if (risltCasuale > risltCasualeBot) {
